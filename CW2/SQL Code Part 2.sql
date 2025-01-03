f9b3ddfc32853c3e726ddb5bdf72bc50c996769d
FantasyLemon <jacob.owalker6@gmail.com>
2025-01-03 11:11:10 +0000

    dropping tables

--- CW2/SQL Code Part 2.sql
+++ CW2/SQL Code Part 2.sql
@@ -1,3 +1,11 @@
+ALTER TABLE CW2.routes DROP CONSTRAINT FK_routes_trail_id;
+ALTER TABLE CW2.trail_features DROP CONSTRAINT FK_trail_features_trail_id;
+
+DROP TABLE IF EXISTS CW2.routes;
+DROP TABLE IF EXISTS CW2.trail_features;
+DROP TABLE IF EXISTS CW2.trails;
+
+
 CREATE TABLE CW2.trails (
     trail_id INT PRIMARY KEY,
     trail_name NVARCHAR(100),
@@ -11,12 +19,12 @@ CREATE TABLE CW2.routes (
     route_id INT PRIMARY KEY,
     trail_id INT,
     route_type NVARCHAR(50),
-    FOREIGN KEY (trail_id) REFERENCES trails(trail_id)
+    FOREIGN KEY (trail_id) REFERENCES CW2.trails(trail_id)
 );
 
 CREATE TABLE CW2.trail_features (
     feature_id INT PRIMARY KEY,
     trail_id INT,
     feature_name NVARCHAR(100),
-    FOREIGN KEY (trail_id) REFERENCES trails(trail_id)
+    FOREIGN KEY (trail_id) REFERENCES CW2.trails(trail_id)
 );
